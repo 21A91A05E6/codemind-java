d8c9279df4c03894fcb7172a2d5434005be5d0f8
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-10-02 17:49:27 +0530

    import java.util.*; class main{     public static void main(String[] args) {         Scanner sc= new Scanner(System.in);         int num1=sc.nextInt();         int num2=sc.nextInt();         System.out.printf("%.4f",(num1+num2)/2.0);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class main{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        System.out.printf("%.4f",(num1+num2)/2.0);
+    }
+}
