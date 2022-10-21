bb5361906cfa3d28ffb1f8919c47d982ed2a64f9
21A91A05E6 <97424463+21A91A05E6@users.noreply.github.com>
2022-10-21 21:51:47 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();
            int[] arr=new int[100];
            int c=0;
            for(int i=0;i<n;i++)
            {
                arr[i]=sc.nextInt();
            }
            int a=sc.nextInt();
            for(int i=0;i<n;i++)
            {
                if(arr[i]==a)
                {
                    c+=1;
                    System.out.print("True");
                    break;
                }
            }
            if(c==0)
            {
                System.out.print("False");
            }
        }
    }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int[] arr=new int[100];
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==a)
+            {
+                c+=1;
+                System.out.print("True");
+                break;
+            }
+        }
+        if(c==0)
+        {
+            System.out.print("False");
+        }
+    }
+}
